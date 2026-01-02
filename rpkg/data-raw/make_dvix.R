6e7dbfff3254bd7d60dac7b0cc9f7065a2639d7b
Steven Pav <shabbychef@gmail.com>
2026-01-01 16:34:29 -0800

    update to new locations of CBOE data
    and remove vxo

--- rpkg/data-raw/make_dvix.R
+++ rpkg/data-raw/make_dvix.R
@@ -27,31 +27,36 @@ suppressMessages({
 #vix.xts <- vix.xts[,'Adjusted Close']
 #colnames(vix.xts) <- 'VIX'
 
-
-vixy <- readr::read_csv('https://ww2.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv',
-												skip=1,
-												col_types=readr::cols(Date=col_date(format='%m/%d/%Y'))
+vixy <- readr::read_csv('https://cdn.cboe.com/api/global/us_indices/daily_prices/VIX_History.csv',
+												skip=0,
+												col_types=readr::cols(DATE=col_date(format='%m/%d/%Y'),.default=col_number())
 												) %>%
 	setNames(gsub('\\s+','_',names(.)))
 #vxo <- readxl::read_xls('http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixarchive.xls',
 												#skip=1) %>%
 	#setNames(gsub('\\s+','_',names(.)))
 
-GET('https://ww2.cboe.com/publish/scheduledtask/mktdata/datahouse/vixarchive.xls', write_disk(tfname <- tempfile(fileext = ".xls")))
-vxo <- readxl::read_xls(tfname, skip=1) %>%
-	setNames(gsub('\\s+','_',names(.))) %>%
-	select(Date,VIX_Close) %>%
-	rename(VIX=VIX_Close) %>%
-	mutate(VIX=as.numeric(VIX)) %>%
-	mutate(Date=as.Date(Date)) %>%
-	filter(!is.na(VIX))
+# this is how you would get "VXO", the original VIX, with a different
+# methodology. Cut it for now.
+#GET('https://cdn.cboe.com/resources/us/indices/vxoarchive.xls',
+		#write_disk(tfname <- tempfile(fileext = ".xls")))
+#vxo <- readxl::read_xls(tfname, skip=2) %>%
+	#setNames(gsub('\\s+','_',names(.))) %>%
+	#select(Date,Close) %>%
+	#rename(VIX=Close) %>%
+	#mutate(VIX=as.numeric(VIX)) %>%
+	#mutate(Date=as.Date(Date)) %>%
+	#filter(!is.na(VIX))
 
-vix <- rbind(vxo, vixy %>% select(Date,VIX_Close) %>% rename(VIX=VIX_Close)) %>%
-	filter(Date <= as.Date('2025-11-30'))
+#vix <- rbind(vxo, vixy %>% select(DATE,CLOSE) %>% rename(Date=DATE,VIX=CLOSE)) 
+
+vix <- vixy %>% select(DATE,CLOSE) %>% rename(Date=DATE,VIX=CLOSE) 
+vix <- vix %>%
+	dplyr::filter(Date <= as.Date('2025-11-30'))
 
 dvix <- xts(x=vix$VIX,order.by=as.Date.POSIXlt(as.POSIXlt(vix$Date)))
 colnames(dvix) <- c('VIX')
-cat('dvix is ',dim(dvix),'\n')
+cat('dvix is',dim(dvix),'through',as.character(tail(index(dvix),1)),'\n')
 
 usethis::use_data(dvix,overwrite=TRUE)
 
